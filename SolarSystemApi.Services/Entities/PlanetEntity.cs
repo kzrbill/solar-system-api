43dc1c6acd919a3a60d4d93dae96fa281420be90
Will Grange <will.grange@gmail.com>
2017-12-01 09:11:00 +0000

    Seeding fuller planet data

--- SolarSystemApi.Services/Entities/PlanetEntity.cs
+++ SolarSystemApi.Services/Entities/PlanetEntity.cs
@@ -7,5 +7,12 @@ namespace SolarSystemApi.Services
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Key { get; set; }
+        public string ImageUrl { get; set; }
+        public long MinDistanceFromSolKm { get; set; }
+        public long MaxDistanceFromSolKm { get; set; }
+        public string MassKg { get; set; }
+        public uint DiameterKm { get; set; }
+        public uint TemperatureMeanKelvin { get; set; }
+        public string VolumeKm3 { get; set; }
     }
 }
